ee9926b51c0e478777ae944d987036e3a3cb5e64
Marsel Zaripov <marszaripov@gmail.com>
2014-04-11 00:07:13 +0400

    add map to main page

--- imh/static/js/controllers.js
+++ imh/static/js/controllers.js
@@ -5,5 +5,10 @@ var imhControllers = angular.module('imhControllers', []);
 imhControllers.controller('MapCtrl', [
     '$scope',
     function ($scope) {
-        $scope.data = 'hello world';
+        var mapOptions = {
+            center: new google.maps.LatLng(55.792403, 49.131203),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     }]);
