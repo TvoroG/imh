3dacc3dc9736dd259506ab3c7873dd9906483ad7
Marsel Zaripov <marszaripov@gmail.com>
2014-04-20 01:44:47 +0400

    get database info from env var in heroku

--- imh/configs/settings.py
+++ imh/configs/settings.py
@@ -1,9 +1,14 @@
 import os
+from urlparse import urlparse
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-# DB, override in local configs
-SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/dbname'
+try:
+    db_uri = urlparse(os.environ['DATABASE_URL'])
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + db_uri.netloc + db_uri.path
+except KeyError as e:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/dbname'
 
 WTF_CSRF_ENABLED = False
